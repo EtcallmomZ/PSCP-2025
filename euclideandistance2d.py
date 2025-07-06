789e9cdb2edf411f174859fcc6fe2bd97f3b40ce
eiweiw321 <eiweiw321@gmail.com>
2025-07-06 15:50:01 +0700

    create a project

---
+++ euclideandistance2d.py
@@ -0,0 +1,10 @@
+"""blabla"""
+import math
+def main():
+    """input"""
+    q1 = float(input())
+    q2 = float(input())
+    p1 = float(input())
+    p2 = float(input())
+    print(math.sqrt((q1-p1)**2 + (q2-p2)**2))
+main()
