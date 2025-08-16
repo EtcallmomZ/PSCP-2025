fd6b115e46f7162701f9424630d867e8baca368a
eiweiw321 <eiweiw321@gmail.com>
2025-08-16 23:27:36 +0700

    update

--- midterm_pscp(mock)/digital.py
+++ midterm_pscp(mock)/digital.py
@@ -8,14 +8,12 @@ def main():
     salary = float(input())
     fak =  float(input())
 
-    if (nation in ("Yes","True") ) and (house in ("Yes","True")):
-        if age >= 16 and fak <= 500000:
-            if salary <= 840000:
-                print(f"Congratulations! {name} is qualified to receive a digital wallet amount of 10,000 baht,sponsored by all taxpayers in Thailand.")
-            else:
-                print(f"Unfortunately, {name} is not qualified.")
-        else:
-            print(f"Unfortunately, {name} is not qualified.")
+    nation = nation.lower()
+    house = house.lower()
+
+
+    if (nation in ("yes","true") ) and (house in ("yes","true")) and age >= 16 and fak <= 500000 and salary <= 840000:
+        print(f"Congratulations! {name} is qualified to receive a digital wallet amount of 10,000 baht, sponsored by all taxpayers in Thailand.")
     else:
         print(f"Unfortunately, {name} is not qualified.")
 main()
