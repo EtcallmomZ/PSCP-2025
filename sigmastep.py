023ac130e8532c8a541ecb2e372d8ce09ce3390b
eiweiw321 <eiweiw321@gmail.com>
2025-09-01 19:02:20 +0700

    add file

---
+++ sigmastep.py
@@ -0,0 +1,13 @@
+"""sigmastep"""
+def main():
+    """input"""
+    x = int(input())
+    y = int(input())
+    n = int(input())
+    total = 0
+    for i in range(x,y,n):
+        total += i
+
+    print(i)
+
+main()
