a12c45f248d1fd77b553da2471a19a93c0b5a5ce
eiweiw321 <eiweiw321@gmail.com>
2025-09-05 22:33:09 +0700

    update file

--- gcd_n.py
+++ gcd_n.py
@@ -1,14 +1,16 @@
 """GCD_N"""
+import math
 def main():
     """input"""
     n = int(input())
-    number = []
-    result = ""
+    t_1 = []
     for _ in range(n):
         x = int(input())
-        number.append(x)
+        t_1.append(x)
 
+    gcd_result = t_1[0]
+    for i in t_1[1:]:
+        gcd_result = math.gcd(gcd_result,i)
 
-
-
+    print(gcd_result)
 main()
