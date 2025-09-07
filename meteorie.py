57c07f61a24ff3c53dbb4b9da084f7e7015da1a5
eiweiw321 <eiweiw321@gmail.com>
2025-09-07 22:07:30 +0700

    update file

--- meteorie.py
+++ meteorie.py
@@ -4,13 +4,20 @@ def main():
     a = float(input())
     b = int(input())
     c = float(input())
-    count = 1
-    kg = a//b
-    while kg >= c:
-        kg //= b
-        count += b
-        
 
-    print(count)
+    if a < c or not a:
+        print(0)
+        return
+
+    d = 0
+    pow_b = 1
+
+    while a >= c*pow_b:
+        d += 1
+        pow_b *= b
+
+    missiles = (b**d-1)/(b-1)
+    print(int(missiles))
+
 
 main()
