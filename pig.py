d1ca544f08bf400997e75fea9bc6e3201ad3cb7c
eiweiw321 <eiweiw321@gmail.com>
2025-09-07 18:13:48 +0700

    update pig

--- pig.py
+++ pig.py
@@ -5,15 +5,13 @@ def pig():
     t_1 = input().split(" ")
     t_1 = list(map(int, t_1))
 
-    t_2 = []
     max_number = []
 
     for i in range(n):
         number = (t_1[2*i], t_1[2*i+1])
-        t_2.append(number)
         max_number.append(max(number))
 
-    if len(t_2) == 1:
+    if n == 1:
         print(max_number[0])
     else:
         result = " + ".join(map(str, max_number))
