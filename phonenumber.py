ae5425e6cf0c4a7c8e664b61e36e6a03801d1eb0
eiweiw321 <eiweiw321@gmail.com>
2025-09-16 21:59:32 +0700

    update new file

--- phonenumber.py
+++ phonenumber.py
@@ -1,8 +1,19 @@
 """phone number"""
 def main():
     """input"""
-    x = int(input())
+    x = input()
     y = input()
+    result = ""
+    lenght = len(x)
+    if lenght == 9:
+        result += f"{x[0]} {x[1:5]} {x[5:]}"
+    else:
+        result += f"{x[0:2]} {x[2:6]} {x[6:]}"
 
+    if y == "International":
+        result = result.replace("0","+66",1)
+        print(result)
+    else:
+        print(result)
 
 main()
