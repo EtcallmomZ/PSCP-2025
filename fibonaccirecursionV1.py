3f7688a7ade86971ed4f38739bab9ec1f91cf2a6
eiweiw321 <eiweiw321@gmail.com>
2025-09-22 20:08:08 +0700

    fibonacci

---
+++ fibonaccirecursionV1.py
@@ -0,0 +1,11 @@
+"""FibonacciRecursionV1"""
+def fibonacci(n):
+    """input"""
+    if not n:
+        return 0
+    if n == 1:
+        return 1
+
+    return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(int(input())))
