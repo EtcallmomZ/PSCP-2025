fe2e5126295a6990954dccdbafa99ea30d3a4cdd
eiweiw321 <eiweiw321@gmail.com>
2025-09-28 20:02:49 +0700

    update right_arrow.py

--- right_arrow.py
+++ right_arrow.py
@@ -4,13 +4,11 @@ def main():
     k = int(input())
     n = int(input())
 
-    mid = (n + 1) // 2
 
-    for i in range(1, n + 1):
-        num_spaces = abs(i - mid)
-        num_stars = k
-        
-        print(" " * num_spaces + "*" * num_stars)
+    for i in range(n//2 + 1):
+        print(" " * i + "*" * k)
+
+    for i in range(n//2 -1 ,-1,-1):
+        print(" " * i + "*" * k)
 
 main()
- 
