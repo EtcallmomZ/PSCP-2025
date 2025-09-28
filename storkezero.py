f7e481c2e21f127031b16ed654de8e952537c807
eiweiw321 <eiweiw321@gmail.com>
2025-09-28 20:14:33 +0700

    update file

--- storkezero.py
+++ storkezero.py
@@ -2,8 +2,15 @@
 def zero():
     """ input """
     n = int(input())
-    for i in range(n):
-        for j in range(i):
 
+    for i in range(1, n + 1):
+        row_numbers = []
+        for j in range(1, i + 1):
+            if i == n or j == 1 or j == i:
+                row_numbers.append("0")
+            else:
+                row_numbers.append("1")
+
+        print(" ".join(row_numbers))
 
 zero()
