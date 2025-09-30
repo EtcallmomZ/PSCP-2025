b810873e7c7af79d5efa558472d1c2055e0d653d
eiweiw321 <eiweiw321@gmail.com>
2025-09-30 22:01:01 +0700

    update sigmastep.py

--- sigmastep.py
+++ sigmastep.py
@@ -5,9 +5,17 @@ def main():
     y = int(input())
     n = int(input())
     total = 0
-    for i in range(x,y,n):
-        total += i
-
-    print(i)
+    if x == y:
+        print(x)
+    elif x < y and n >= 1:
+        for i in range(x,y+1,n):
+            total += i
+        print(total)
+    elif x > y and n < 0:
+        for i in range(x,y-1,n):
+            total += i
+        print(total)
+    else:
+        print("error")
 
 main()
